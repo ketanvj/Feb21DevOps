4bc696e9064eafcb5a23500bff4411191fe23f74
Ketan Jobanputra <abc.yahoo.com>
2021-02-14 11:59:15 +0530

    abc.txt

--- Shipment.java
+++ Shipment.java
@@ -1,2 +1,5 @@
 This my second file inside my first repo
 This my second file inside my first repo and second line.
+git pull = fetch from server + merge with local
+
+git fetch - Which only fetches and merging has to be done through merge command.
